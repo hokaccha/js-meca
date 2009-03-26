8ce039ee3a5519091e58521c464e4eafa784f2a4
hokaccha <k.hokamura@gmail.com>
2009-03-27 08:46:46 +0900

    add module heightAlign

--- meca.js
+++ meca.js
@@ -1,12 +1,12 @@
 /*
- * meca.js 1.0.4 markup engineer's coding adminicle javascript library
+ * meca.js 1.1.0 markup engineer's coding adminicle javascript library
  *
  * Copyright (c) 2009 Kazuhito Hokamura
  * Licensed under the MIT License:
  * http://www.opensource.org/licenses/mit-license.php
  *
  * @author   Kazuhito Hokamura (http://webtech-walker.com/)
- * @version  1.0.4
+ * @version  1.1.0
  * @url      http://webtech-walker.com/meca/
  *
  */
@@ -14,59 +14,71 @@
 (function($) {
     $.Meca = {};
 
-    $.Meca.config = {
-        /*
-         * image hover config setting
-         */ 
+    /*
+     * config setting
+     -----------------------------------*/
 
-        // enable hover image. true or false
-        hoverEnable: true,
+    /**
+     * image hover config setting
+     */ 
 
-        // hover image target selector
-        hoverSelector: '.btn',
+    $.Meca.hover = {};
+    $.Meca.hover.config = {};
 
-        // hover image postfix string
-        hoverPostfix: '_o',
+    $.Meca.hover.config.enable   = true;
+    $.Meca.hover.config.selector = '.btn';
+    $.Meca.hover.config.postfix  = '_o';
 
 
-        /*
-         * external link config setting
-         */
+    /**
+     * external link config setting
+     */
 
-        // enable external link. true or false
-        externalEnable: true,
+    $.Meca.external = {};
+    $.Meca.external.config = {};
 
-        // external link target selector. if externalRel is false then this config enable
-        externalSelector: 'a[rel~="external"]',
+    $.Meca.external.config.enable   = true;
+    $.Meca.external.config.selector = 'a[rel~="external"]';
 
         
-        /*
-         * pngfix config setting
-         */
+    /**
+     * pngfix config setting
+     */
 
-        // enable hover image. true or false
-        pngfixEnable: true,
+    $.Meca.pngfix = {};
+    $.Meca.pngfix.config = {};
 
-        // background png fix target selector
-        bgpngfixSelector: '.bgpng',
+    $.Meca.pngfix.config.enable         = true;
+    $.Meca.pngfix.config.bgpngSelector  = '.bgpng';
+    $.Meca.pngfix.config.imgpngSelector = '.pngfix';
+    $.Meca.pngfix.config.blankGif       = false;
+    $.Meca.pngfix.config.wrapSpanClass  = 'imgpngWrapSpan';
 
-        // img element png fix target selector
-        imgpngSelector: '.pngfix',
+    /**
+     * heightAlign config setting
+     */
 
-        // blank.gif path. if value is false then img elem wrap span
-        imgpngBlankGif: false,
+    $.Meca.heightAlign = {};
+    $.Meca.heightAlign.config = {};
 
-        // wrap span class that when blank.gif is false
-        imgpngWrapSpanClass: 'imgpngWrapSpan'
+    $.Meca.heightAlign.config.enable   = true;
+    $.Meca.heightAlign.config.selector = 'ul.heightAlign li';
 
-    };
 
-    // hover image
-    $.Meca.hover = function() {
-        if (!$.Meca.config.hoverEnable) return;
-        $($.Meca.config.hoverSelector).each(function() {
+    /*
+     * exec modules
+     -----------------------------------*/
+
+    $.Meca.exec = {};
+
+    /**
+     * hover image
+     */
+    $.Meca.hover.exec = function() {
+        if (!$.Meca.hover.config.enable) return;
+        $($.Meca.hover.config.selector).each(function() {
             var src   = this.src;
-            var src_o = this.src.replace(/\.\w+$/, $.Meca.config.hoverPostfix + '$&');
+            var src_o = this.src.replace(/\.\w+$/, $.Meca.hover.config.postfix + '$&');
             var img   = new Image();
             img.src   = src_o;
             $(this).hover(
@@ -76,15 +88,21 @@
         });
     };
 
-    // external link
-    $.Meca.external = function() {
-        if (!$.Meca.config.externalEnable) return;
-        $($.Meca.config.externalSelector).attr('target', '_blank');
+
+    /**
+     * external link
+     */
+    $.Meca.external.exec = function() {
+        if (!$.Meca.external.config.enable) return;
+        $($.Meca.external.config.selector).attr('target', '_blank');
     };
 
-    // alpha png image for IE6
-    $.Meca.pngfix = function() {
-        if (!$.Meca.config.pngfixEnable) return;
+
+    /**
+     * alpha png image for IE6
+     */
+    $.Meca.pngfix.exec = function() {
+        if (!$.Meca.pngfix.config.enable) return;
         if (!($.browser.msie && $.browser.version == "6.0")) return;
 
         var getFilterStyle = function(src, sizing) {
@@ -93,7 +111,7 @@
         };
 
         // background png to alpha png
-        $($.Meca.config.bgpngfixSelector).each(function() {
+        $($.Meca.pngfix.config.bgpngSelector).each(function() {
             var bgpngSrc    = $(this).css('backgroundImage').slice(5,-2);
             var bgpngSizing = ($(this).css('backgroundRepeat') === 'no-repeat') ? 'crop' : 'scale';
             var bgpngStyle  = {
@@ -105,7 +123,7 @@
         });
 
         // img element png to alpha png
-        $($.Meca.config.imgpngSelector).each(function() {
+        $($.Meca.pngfix.config.imgpngSelector).each(function() {
             var imgpngStyle  = {
                 'filter': getFilterStyle($(this).attr('src'), 'crop'),
                 'width':  $(this).width(),
@@ -113,22 +131,37 @@
                 'zoom':   '1'
             };
 
-            if ($.Meca.config.imgpngBlankGif) {
-                $(this).css(imgpngStyle).attr('src', $.Meca.config.imgpngBlankGif);
+            if ($.Meca.pngfix.config.blankGif) {
+                $(this).css(imgpngStyle).attr('src', $.Meca.pngfix.config.blankGif);
             }
             else {
                 var wrapSpan = $(document.createElement('span'))
-                    .addClass($.Meca.config.imgpngWrapSpanClass)
+                    .addClass($.Meca.pngfix.config.wrapSpanClass)
                     .css(imgpngStyle);
                 $(this).css('display', 'none').wrap(wrapSpan);
             }
         });
+    };
 
+    /**
+     * heightAlign adujst
+     */
+    $.Meca.heightAlign.exec = function() {
+        if (!$.Meca.heightAlign.config.enable) return;
+        var maxHeight = 0;
+        $($.Meca.heightAlign.config.selector).each(function() {
+            height = $(this).height();
+            if (maxHeight < height) {
+                maxHeight = height;
+            }
+        }).height(maxHeight);
     };
 
     $(function() {
-        $.Meca.hover();
-        $.Meca.external();
-        $.Meca.pngfix();
+        for (var module in $.Meca) {
+            if ($.Meca[module].exec) {
+                $.Meca[module].exec();
+            }
+        }
     });
 })(jQuery);
