414ea9b42a252fd5f6ad4c0b4bb2004ad6a46619
hokaccha <k.hokamura@gmail.com>
2010-06-09 23:23:39 +0900

    サンプル追加

--- sample/js/page_state.js
+++ sample/js/page_state.js
@@ -1,17 +1,15 @@
-$(function() {
-    $('.content').show();
-    return;
-    $('#menu a').click(function() {
-        var elem = $(this);
-        var page_id = elem.attr('href');
-
-        $('section.content').hide();
-        $(elem.attr('href')).show();
-
-        location.hash = page_id;
-        window.scroll(0, 0);
-        return false;
-    });
-
-    $(location.hash || '#top').show();
-});
+//$(function() {
+//    $('#menu a').click(function() {
+//        var elem = $(this);
+//        var page_id = elem.attr('href');
+//
+//        $('section.content').hide();
+//        $(elem.attr('href')).show();
+//
+//        location.hash = page_id;
+//        window.scroll(0, 0);
+//        return false;
+//    });
+//
+//    $(location.hash || '#top').show();
+//});
